2cd05eeddf63993c4fa466a4b34b492ed699c939
Ian Hunter <ijh.hunter@gmail.com>
2019-08-03 01:53:57 -0400

    Project setup

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,33 @@
 <template>
+  <!-- App.vue -->
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+    <v-navigation-drawer app clipped>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <!-- -->
     </v-app-bar>
 
+    <!-- Sizes your content based upon application components -->
     <v-content>
-      <HelloWorld/>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+  name: 'App'
+}
 </script>
